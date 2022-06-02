d0bfc1d365ef71489b05dc91dcfa71e50a6f10d7
Matthew J Pearson <seaofcode@gmail.com>
2022-06-02 14:35:59 -0500

    String Challenge 02

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-// Statments and Comments
+// String Concatenation - combine string values
+// `` - backticks (template strings) easier option 
 
-// document.write('hello world');
-// console.log('hello world');
+const firstName = 'shake';
+const lastName = 'and bake';
+let fullName = firstName + ' ' + lastName;
 
-// Variables 
-// Declare and Assign
-// let myName = "Matthew"; 
-// console.log(myName);
-
-// let address, zip, state;
-// address = "144 main street";
-// zip = '34323';
-// state = 'TX';
-// console.log(address,zip,state);
-
-// can contain digits, letters, underscore, $
-//  must start with letter, $ or _
-// no keyword
-// cannot start with number
-// case sensitive - fullname vs Fullname
-// camelCase or underscore
-
-
+console.log('hello there ' + fullName);
---
+++ js-challenges/02-string.js
@@ -0,0 +1,16 @@
+// ## Concatenate Strings #2
+
+// 1. create "street" and "country" variables
+// 2. assign your values
+// 3. create "fullMailingAddress" variable and assign
+//    the result of "street + country"
+// 4. remember about the space
+// 5. log "fullMailingAddress" in the console
+
+const street = 'Redwall';
+const country = 'Hillside';
+
+let fullMailingAddress = street + ' ' + country;
+
+console.log(fullMailingAddress);
+document.write(fullMailingAddress);
